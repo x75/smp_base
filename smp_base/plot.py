9455ab5459671446db3f38de9ceaacebaa7d1281
Oswald Berthold <opt@sdfk.de>
2017-05-26 13:24:46 +0200

    soft import for pyunicorn and seaborn

--- smp_base/plot.py
+++ smp_base/plot.py
@@ -11,15 +11,42 @@ import matplotlib.gridspec as gridspec
 
 import numpy as np
 
-from pyunicorn.timeseries import RecurrencePlot
+try:
+    from pyunicorn.timeseries import RecurrencePlot
+    HAVE_PYUNICORN = True
+except ImportError, e:
+    print "Couldn't import RecurrencePlot from pyunicorn.timeseries, make sure pyunicorn is installed", e
+    HAVE_PYUNICORN = False
 
-import seaborn as sns
+try:
+    import seaborn as sns
+    HAVE_SEABORN = True
+except ImportError, e:
+    print "Couldn't import seaborn as sns, make sure seaborn is installed", e
+    HAVE_SEABORN = False
 
 import pandas as pd
 from pandas.tools.plotting import scatter_matrix
 
+def make_axes_from_grid(fig, gs):
+    axes = []
+    (rows, cols) = gs.get_geometry()
+    for row in range(rows):
+        axes.append([])
+        for col in range(cols):
+            axes[-1].append(fig.add_subplot(gs[row, col]))
+    return axes
 
-def makefig(rows = 1, cols = 1, wspace = 0.0, hspace = 0.0):
+def make_axes_from_spec(fig, gs, axesspec):
+    axes = []
+    # (rows, cols) = gs.get_geometry()
+    for axspec in axesspec:
+        axes.append([])
+        # for col in range(cols):
+        axes[-1].append(fig.add_subplot(gs[axspec]))
+    return axes
+
+def makefig(rows = 1, cols = 1, wspace = 0.0, hspace = 0.0, axesspec = None):
     """create figure and subplot structure
 
 return figure handle
@@ -29,12 +56,12 @@ return figure handle
     fig = plt.figure()
     gs = gridspec.GridSpec(rows, cols)
 
-    axes = []
 
-    for row in range(rows):
-        axes.append([])
-        for col in range(cols):
-            axes[-1].append(fig.add_subplot(gs[row, col]))
+    if axesspec is None:
+        axes = make_axes_from_grid(fig, gs)
+    else:
+        axes = make_axes_from_spec(fig, gs, axesspec)
+        
     # print "fig.axes", fig.axes
     plt.subplots_adjust(wspace = wspace, hspace = hspace)
     # plt.subplots_adjust(wspace=0.1, hspace = 0.3)
@@ -86,6 +113,8 @@ def timeseries(ax, data, **kwargs):
 
 def histogram(ax, data, **kwargs):
     """histogram plot"""
+    assert len(data.shape) > 0
+    # print "histo data", data
     # style params
     # axis title
     if kwargs.has_key('title'):
@@ -93,27 +122,31 @@ def histogram(ax, data, **kwargs):
     else:
         title = 'histogram of %s-shaped data, log-scale' % data.shape
 
-    ax.hist(data, bins = int(np.log(data.shape[0]/2)), alpha = 0.5)
+    ax.hist(data, bins = int(np.log(max(3, data.shape[0]/2))), alpha = 0.5)
     ax.set_yscale('log')
     ax.title.set_text(title)
     ax.title.set_fontsize(8.0)
 
-def rp_timeseries_embedding(ax, data, **kwargs):
-    """recurrence plot"""
-    emb_del = 1
-    emb_dim = 10
-    # make data "strictly" one-dimensional
-    data = data.reshape((-1, ))
-    rp = RecurrencePlot(time_series = data, tau = emb_del, dim = emb_dim, threshold_std = 1.5)
+if HAVE_PYUNICORN:
+    def rp_timeseries_embedding(ax, data, **kwargs):
+        """recurrence plot"""
+        emb_del = 1
+        emb_dim = 10
+        # make data "strictly" one-dimensional
+        data = data.reshape((-1, ))
+        rp = RecurrencePlot(time_series = data, tau = emb_del, dim = emb_dim, threshold_std = 1.5)
 
-    plotdata = rp.recurrence_matrix()
-    length = plotdata.shape[0]
+        plotdata = rp.recurrence_matrix()
+        length = plotdata.shape[0]
     
-    xs = np.linspace(0, length, length)
-    ys = np.linspace(0, length, length)
-    ax.pcolormesh(xs, ys, plotdata, cmap=plt.get_cmap("Oranges"))
-    ax.set_xlabel("$n$")
-    ax.set_ylabel("$n$")
+        xs = np.linspace(0, length, length)
+        ys = np.linspace(0, length, length)
+        ax.pcolormesh(xs, ys, plotdata, cmap=plt.get_cmap("Oranges"))
+        ax.set_xlabel("$n$")
+        ax.set_ylabel("$n$")
+else:
+    def rp_timeseries_embedding(ax, data, **kwargs):
+        print "Dummy, pyunicorn not installed"
 
 # visualization of multi-dimensional data
 # check
@@ -125,37 +158,41 @@ def rp_timeseries_embedding(ax, data, **kwargs):
 # smp/plot?
 # smp/actinf: has dimstack etc ...
 # evoplast
-# ...? 
-def histogramnd(ax, data, **kwargs):
-    scatter_data_raw  = data
-    scatter_data_cols = ["x_%d" % (i,) for i in range(data.shape[1])]
+# ...?
+if HAVE_SEABORN:
+    def histogramnd(ax, data, **kwargs):
+        scatter_data_raw  = data
+        scatter_data_cols = ["x_%d" % (i,) for i in range(data.shape[1])]
 
-    # prepare dataframe
-    df = pd.DataFrame(scatter_data_raw, columns=scatter_data_cols)
+        # prepare dataframe
+        df = pd.DataFrame(scatter_data_raw, columns=scatter_data_cols)
         
-    g = sns.PairGrid(df)
-    # g.map_diag(plt.hist)
-    g.map_diag(sns.kdeplot)
-    g.map_offdiag(plt.hexbin, cmap="gray", gridsize=30, bins="log");
+        g = sns.PairGrid(df)
+        # g.map_diag(plt.hist)
+        g.map_diag(sns.kdeplot)
+        g.map_offdiag(plt.hexbin, cmap="gray", gridsize=30, bins="log");
 
-    # print "dir(g)", dir(g)
-    # print g.diag_axes
-    # print g.axes
+        # print "dir(g)", dir(g)
+        # print g.diag_axes
+        # print g.axes
     
-    # for i in range(data.shape[1]):
-    #     for j in range(data.shape[1]): # 1, 2; 0, 2; 0, 1
-    #         if i == j:
-    #             continue
-    #         # column gives x axis, row gives y axis, thus need to reverse the selection for plotting goal
-    #         # g.axes[i,j].plot(df["%s%d" % (self.cols_goal_base, j)], df["%s%d" % (self.cols_goal_base, i)], "ro", alpha=0.5)
-    #         g.axes[i,j].plot(df["x_%d" % (j,)], df["x_%d" % (i,)], "ro", alpha=0.5)
+        # for i in range(data.shape[1]):
+        #     for j in range(data.shape[1]): # 1, 2; 0, 2; 0, 1
+        #         if i == j:
+        #             continue
+        #         # column gives x axis, row gives y axis, thus need to reverse the selection for plotting goal
+        #         # g.axes[i,j].plot(df["%s%d" % (self.cols_goal_base, j)], df["%s%d" % (self.cols_goal_base, i)], "ro", alpha=0.5)
+        #         g.axes[i,j].plot(df["x_%d" % (j,)], df["x_%d" % (i,)], "ro", alpha=0.5)
                 
-    plt.show()
+        plt.show()
 
     
-    # run sns scattermatrix on dataframe
-    # plot_scattermatrix(df, ax = None)
-
+        # run sns scattermatrix on dataframe
+        # plot_scattermatrix(df, ax = None)
+else:
+    def histogramnd(ax, data, **kwargs):
+        print "Dummy, seaborn not installed"
+        
 def plot_scattermatrix(df, **kwargs):
     """plot a scattermatrix of dataframe df"""
     if df is None:
