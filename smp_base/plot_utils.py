2379466651d20e9af20adaa9d7844972973d7fc7
Oswald Berthold <bertolos@informatik.hu-berlin.de>
2018-02-13 20:03:46 +0100

    added handles arg to custom_legend to control legend from conf with label and line idx

--- smp_base/plot_utils.py
+++ smp_base/plot_utils.py
@@ -35,27 +35,29 @@ def resize_panel_horiz(resize_by = 0.8, ax = None):
     box = ax.get_position()
     ax.set_position([box.x0, box.y0, box.width, box.height * resize_by])
 
-def custom_legend(labels = None, resize_by = 0.8, ax = None, loc = 'right', lg = None):
+def custom_legend(labels = None, handles = None, resize_by = 0.8, ax = None, loc = 'right', lg = None):
     if loc == 'right' or loc == 'left':
-        put_legend_out_right(labels = labels, resize_by = resize_by, ax = ax, right = loc, lg = lg)
+        put_legend_out_right(labels = labels, handles=handles, resize_by = resize_by, ax = ax, right = loc, lg = lg)
     elif loc in ['upper', 'top'] or loc == 'lower':
-        put_legend_out_top(labels = labels, resize_by = resize_by, ax = ax, top = loc, lg = lg)
+        put_legend_out_top(labels = labels, handles=handles, resize_by = resize_by, ax = ax, top = loc, lg = lg)
     elif type(loc) is tuple:
-        put_legend_out(labels = labels, resize_by = resize_by, ax = ax, loc = loc, lg = lg)
+        put_legend_out(labels = labels, handles=handles, resize_by = resize_by, ax = ax, loc = loc, lg = lg)
 
-def put_legend_out(labels = None, resize_by = 0.8, ax = None, loc = None, lg = None):
+def put_legend_out(labels = None, handles=None, resize_by = 0.8, ax = None, loc = None, lg = None):
     ax = ax_check(ax)
     if loc[0] < 0.1 or loc[0] > 0.9:
         resize_panel_vert(resize_by = resize_by, ax = ax)
     if loc[1] < 0.1 or loc[1] > 0.9:
         resize_panel_horiz(resize_by = resize_by, ax = ax)
         
-    if labels is None:
+    if labels is None and handles is None:
         ax.legend(loc = loc, ncol=1)
-    else:
+    elif labels is not None and handles is None:
         ax.legend(loc = loc, ncol=1, labels = labels)
-
-def put_legend_out_right(labels = None, resize_by = 0.8, ax = None, right = 'left', lg = None):
+    else:
+        ax.legend(loc = loc, ncol=1, labels = labels, handles=handles)
+        
+def put_legend_out_right(labels = None, handles=None, resize_by = 0.8, ax = None, right = 'left', lg = None):
     ax = ax_check(ax)
     resize_panel_vert(resize_by = resize_by, ax = ax)
     loc = 'upper %s' % (right, )
@@ -70,12 +72,14 @@ def put_legend_out_right(labels = None, resize_by = 0.8, ax = None, right = 'lef
         bbox = (-0.25, bboxy)
     # loc = 'center left'
     # bbox = (1.0, 0.5)
-    if labels is None:
+    if labels is None and handles is None:
         ax.legend(loc = loc, bbox_to_anchor = bbox, ncol=1)
-    else:
+    elif labels is not None and handles is None:
         ax.legend(loc = loc, bbox_to_anchor = bbox, ncol=1, labels = labels)
+    else:
+        ax.legend(loc = loc, bbox_to_anchor = bbox, ncol=1, labels = labels, handles=handles)
 
-def put_legend_out_top(labels = None, resize_by = 0.8, ax = None, top = 'lower', lg = None):
+def put_legend_out_top(labels = None, handles=None, resize_by = 0.8, ax = None, top = 'lower', lg = None):
     ax = ax_check(ax)
     resize_panel_horiz(resize_by = resize_by, ax = ax)
     loc = '%s center' % (top, )
@@ -88,8 +92,10 @@ def put_legend_out_top(labels = None, resize_by = 0.8, ax = None, top = 'lower',
         bbox = (bboxx, -0.1) #
     elif top == 'lower':
         bbox = (bboxx, 1.1) #
-    if labels is None:
+    if labels is None and handles is None:
         ax.legend(loc = loc, bbox_to_anchor = bbox, ncol=10)        
-    else:
+    elif labels is not None and handles is None:
         ax.legend(loc = loc, bbox_to_anchor = bbox, ncol=10, labels = labels)
+    else:
+        ax.legend(loc = loc, bbox_to_anchor = bbox, ncol=10, labels = labels, handles=handles)
 
